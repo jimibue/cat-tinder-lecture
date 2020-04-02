6c298f56e18e846b1a137107cc506449e7ece72f
James Yeates <jimibue@gmail.com>
2020-04-02 07:44:43 -0600

    yo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+200.times do
+  name = Faker::Creature::Cat.name
+  breed = Faker::Creature::Cat.breed
+  registry = Faker::Creature::Cat.registry
+  avatar = Faker::Avatar.image(slug: name, size: "100x400", format: "png", set: "set4")
+  Cat.create(name: name, breed: breed, registry: registry, avatar: avatar)
+end
+
+puts "200 Cats Seeded"class AddTrackableToUsers < ActiveRecord::Migration[6.0]
+def change
+    ## Trackable
+    add_column :users, :sign_in_count, :integer, :default => 0
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
+    add_column :users, :current_sign_in_ip, :string
+    add_column :users, :last_sign_in_ip, :string
+end
+end
